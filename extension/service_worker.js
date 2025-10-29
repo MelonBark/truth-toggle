b11d4ef9a7ee7060fcec097b56d073b5fb37a7a9
MelonBark <support+truth-toggle@melonbark.com>
2025-10-28 20:28:59 -0400

    Update action title in manifest and refactor service worker URL handling functions for clarity

--- extension/service_worker.js
+++ extension/service_worker.js
@@ -3,39 +3,44 @@ var grokiIdentifier = 'grokipedia.com/page'
 var igIdentifier = 'infogalactic.com/info'
 var redirectedArray = {}
 
+function isWikipedia(url) {
+  return /^https:\/\/(?:en\.|www\.)?wikipedia\.org\/wiki\//.test(url);
+}
+
+function isGrokipedia(url) {
+  return url.includes('grokipedia.com/page');
+}
+
+function isInfogalactic(url) {
+  return url.includes('infogalactic.com/info');
+}
+
 function replace_url (url, wikipedia_fragment, infogalactic_fragment) {
   return url.replace(new RegExp('https:\\/\\/(|en\\.|www\\.)' + wikipedia_fragment), 'https://' + infogalactic_fragment)
 };
 
-chrome.action.onClicked.addListener(
-  function (tab) {
-    chrome.tabs.query(
-      {active: true, currentWindow: true},
-      function (tabs) {
-        var activeTab = tabs[0]
-        var currentURL = activeTab.url
-        redirectedArray[activeTab.id] = 'disallowRedirect'
+chrome.action.onClicked.addListener(function(tab) {
+  var currentURL = tab.url
+  redirectedArray[tab.id] = 'disallowRedirect'
 
-        if (currentURL.includes(wikiIdentifier)) {
-          var grokiURL = replace_url(currentURL, wikiIdentifier, grokiIdentifier)
-          return chrome.tabs.update({'url': grokiURL})
-        }
-
-        if (currentURL.includes(grokiIdentifier)) {
-          var igURL = currentURL.replace(grokiIdentifier, igIdentifier)
-          return chrome.tabs.update({'url': igURL})
-        }
-
-        if (currentURL.includes(igIdentifier)) {
-          var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
-          return chrome.tabs.update({'url': wikiURL})
-        }
-      })
+  if (isWikipedia(currentURL)) {
+    var grokiURL = replace_url(currentURL, wikiIdentifier, grokiIdentifier)
+    chrome.tabs.update(tab.id, {url: grokiURL})
   }
-)
+
+  if (isGrokipedia(currentURL)) {
+    var igURL = currentURL.replace(grokiIdentifier, igIdentifier)
+    chrome.tabs.update(tab.id, {url: igURL})
+  }
+
+  if (isInfogalactic(currentURL)) {
+    var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
+    chrome.tabs.update(tab.id, {url: wikiURL})
+  }
+})
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.url && changeInfo.url.includes(wikiIdentifier)) {
+  if (changeInfo.url && isWikipedia(changeInfo.url)) {
     if (!(tabId in redirectedArray)) {
       redirectedArray[tabId] = 'allowRedirect';
     }
@@ -44,7 +49,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
       chrome.tabs.update(tabId, {url: grokiURL});
     }
   } else if (changeInfo.status === 'complete') {
-    if (!tab.url.includes(wikiIdentifier) && !tab.url.includes(grokiIdentifier) && !tab.url.includes(igIdentifier)) {
+    if (!isWikipedia(tab.url) && !isGrokipedia(tab.url) && !isInfogalactic(tab.url)) {
       redirectedArray[tabId] = 'allowRedirect';
     }
   }
